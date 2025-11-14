09bd9c81851f0f3b7b2ab2c20826c1bb6e83980d
sooshiance <s.katafract@gmail.com>
2025-11-14 21:07:14 +0330

    feat: Add ChatClient class & first ui settings

--- client.py
+++ client.py
@@ -32,3 +32,42 @@ class ZMQReceiverThread(QThread):
         self.sub = self.context.socket(zmq.SUB)
         self.sub.connect(self.sub_addr)
         self.sub.setsockopt_string(zmq.SUBSCRIBE, "")
+    
+    def run(self):
+        while True:
+            try:
+                raw = self.sub.recv_string()
+                msg = json.loads(raw)
+                self.message_received.emit(msg)
+            except Exception as e:
+                print(str(e))
+                pass
+
+    def stop(self):
+        self.sub.close()
+        self.context.term()
+
+
+class ChatClient(QMainWindow):
+    def __init__(self, username: str, is_admin: bool = False) -> None:
+        super().__init__()
+        self.username = username
+        self.is_admin = is_admin
+        self.groups: dict[str, set[str]] = {}
+        self.joined_groups: set[str] = set()
+        self.private_windows: dict[str, PrivateChatWindow] = {}
+
+        # ZMQ setup
+        self.context = zmq.Context()
+        self.pub = self.context.socket(zmq.PUB)
+        self.pub.connect("tcp://localhost:6001")  # to provider SUB (for sending)
+
+        # Start listening thread
+        self.receiver = ZMQReceiverThread(
+            "tcp://localhost:6000"
+        )  # to provider PUB (for receiving)
+        self.receiver.message_received.connect(self.handle_incoming_message)
+        self.receiver.start()
+
+        self.init_ui()
+        self.refresh_groups()
